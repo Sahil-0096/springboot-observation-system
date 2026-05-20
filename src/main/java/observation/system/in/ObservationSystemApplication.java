4ad0f62184c77a29a9e37af76d04b44fe6ef77da
Sahil <sahilyadav0096@gmail.com>
2026-05-20 22:07:28 +0530

    Initial Spring Boot 4 project setup

---
+++ src/main/java/observation/system/in/ObservationSystemApplication.java
@@ -0,0 +1,13 @@
+package observation.system.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/observation/system/in/ObservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package observation.system.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
